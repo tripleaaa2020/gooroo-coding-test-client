a1106451bf2f7e1d81f06e0efa12ed629247b4e0
angkasa1905 <albert.angkasa@sprout.co.id>
2020-04-14 17:02:56 +0700

    rebuild project

---
+++ build/precache-manifest.5612b3e165c5f7d08a6e4957da81c0d8.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9845de3340039378f3ac7b4fb53b66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3d7ea297a17eb5eca22",
+    "url": "/static/css/10.a3a2e315.chunk.css"
+  },
+  {
+    "revision": "5150ee1ea90534c492ee",
+    "url": "/static/css/11.10ab65ae.chunk.css"
+  },
+  {
+    "revision": "74351894b178f5487f4e",
+    "url": "/static/css/4.3ebd2336.chunk.css"
+  },
+  {
+    "revision": "e424e51d4bcdfd281f32",
+    "url": "/static/css/5.b010643e.chunk.css"
+  },
+  {
+    "revision": "96327ac49308d5ccbc23",
+    "url": "/static/css/6.b2d525cf.chunk.css"
+  },
+  {
+    "revision": "b80043b4b5ff350f618e",
+    "url": "/static/css/9.35f9b352.chunk.css"
+  },
+  {
+    "revision": "3035350c53730e18bc91",
+    "url": "/static/css/main.ec5c0e17.chunk.css"
+  },
+  {
+    "revision": "2ef49c9a5626e97c45ef",
+    "url": "/static/js/0.ad4b140e.chunk.js"
+  },
+  {
+    "revision": "05f21e782627a8c68f6e",
+    "url": "/static/js/1.fd1b963c.chunk.js"
+  },
+  {
+    "revision": "a3d7ea297a17eb5eca22",
+    "url": "/static/js/10.befd0120.chunk.js"
+  },
+  {
+    "revision": "5150ee1ea90534c492ee",
+    "url": "/static/js/11.f539ffeb.chunk.js"
+  },
+  {
+    "revision": "0be1b35080c18107771c",
+    "url": "/static/js/12.6b6f1a8f.chunk.js"
+  },
+  {
+    "revision": "3ea00d507180514de143",
+    "url": "/static/js/13.3bc81f41.chunk.js"
+  },
+  {
+    "revision": "74351894b178f5487f4e",
+    "url": "/static/js/4.4417d788.chunk.js"
+  },
+  {
+    "revision": "9617c35fefb3909e96d4735aba67fb26",
+    "url": "/static/js/4.4417d788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e424e51d4bcdfd281f32",
+    "url": "/static/js/5.757cc686.chunk.js"
+  },
+  {
+    "revision": "96327ac49308d5ccbc23",
+    "url": "/static/js/6.b4fd4b1a.chunk.js"
+  },
+  {
+    "revision": "1f2082101a52c872c3f5",
+    "url": "/static/js/7.912236c5.chunk.js"
+  },
+  {
+    "revision": "c360eff85ca112d098c080b19dff9b78",
+    "url": "/static/js/7.912236c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6329bf9a1b4fed833f01",
+    "url": "/static/js/8.7f46992a.chunk.js"
+  },
+  {
+    "revision": "b80043b4b5ff350f618e",
+    "url": "/static/js/9.f45dd764.chunk.js"
+  },
+  {
+    "revision": "3035350c53730e18bc91",
+    "url": "/static/js/main.0d59502f.chunk.js"
+  },
+  {
+    "revision": "860efcff9ef4b6e80e34",
+    "url": "/static/js/runtime-main.66df71ec.js"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "13f0461d61d9e6ed65665d21bc5fa525",
+    "url": "/static/media/autobio.13f0461d.svg"
+  },
+  {
+    "revision": "9d67da1d33316a50000eba886503710e",
+    "url": "/static/media/back-icon.9d67da1d.svg"
+  },
+  {
+    "revision": "7751e0d21fd516d968501ae5b2e46357",
+    "url": "/static/media/background.7751e0d2.jpg"
+  },
+  {
+    "revision": "f11d32d1590157d1a7be7178fa7f5ed0",
+    "url": "/static/media/book-2.f11d32d1.svg"
+  },
+  {
+    "revision": "610ad648c44ebb1d31086cd8bc938304",
+    "url": "/static/media/book-add-grey.610ad648.svg"
+  },
+  {
+    "revision": "aee1a5448374f6b6946b4465a2292893",
+    "url": "/static/media/chevron-down.aee1a544.svg"
+  },
+  {
+    "revision": "d9cd8e4a52530d2b495afee438faf647",
+    "url": "/static/media/close-popup-icon.d9cd8e4a.svg"
+  },
+  {
+    "revision": "570adf259234030c9486cd7e4e73802e",
+    "url": "/static/media/comedy.570adf25.svg"
+  },
+  {
+    "revision": "98dfd2020cd1a8afe69ca97972b7e8a3",
+    "url": "/static/media/edit-icon-orange.98dfd202.svg"
+  },
+  {
+    "revision": "28bc701a65b100829ae68f7d12eb0e80",
+    "url": "/static/media/fantasy.28bc701a.svg"
+  },
+  {
+    "revision": "30ed8361881044fcc0f8ed70358be045",
+    "url": "/static/media/filter-icon.30ed8361.svg"
+  },
+  {
+    "revision": "26175e8586481640c05c7a44923f5084",
+    "url": "/static/media/horror.26175e85.svg"
+  },
+  {
+    "revision": "76c6fb64e77d6792dc2456879a894293",
+    "url": "/static/media/ink.76c6fb64.svg"
+  },
+  {
+    "revision": "bda4e810ad6a8d4e75b39f5781d6439f",
+    "url": "/static/media/library-grey.bda4e810.svg"
+  },
+  {
+    "revision": "08f2b7d6aba4b58ab3b2f6707bb0ecb5",
+    "url": "/static/media/library-orange.08f2b7d6.svg"
+  },
+  {
+    "revision": "4201213f165d17cfbe2c3611b00bf290",
+    "url": "/static/media/logo.4201213f.svg"
+  },
+  {
+    "revision": "cb8a07e92543e8182d82e03c15d26fb6",
+    "url": "/static/media/logout-grey.cb8a07e9.svg"
+  },
+  {
+    "revision": "70774874f09ce3fef2ac8c986ddd8d10",
+    "url": "/static/media/logout-orange.70774874.svg"
+  },
+  {
+    "revision": "240b4265cf14c183512fcdc6b583d05a",
+    "url": "/static/media/misc.240b4265.svg"
+  },
+  {
+    "revision": "098e91bb1b1c2b1ea5a8fdaec4548a89",
+    "url": "/static/media/mybooks-grey.098e91bb.svg"
+  },
+  {
+    "revision": "f759887931f8aab293ce069498693faf",
+    "url": "/static/media/mybooks-orange.f7598879.svg"
+  },
+  {
+    "revision": "97c34c04d03e279a891a18e32ca1c42b",
+    "url": "/static/media/non-fiction.97c34c04.svg"
+  },
+  {
+    "revision": "31dd455fc07aa8f44f644fd9e67a69b6",
+    "url": "/static/media/nothing.31dd455f.svg"
+  },
+  {
+    "revision": "5b41b434031a531b55567d419011c4d2",
+    "url": "/static/media/publish.5b41b434.svg"
+  },
+  {
+    "revision": "020dfd90eb90fdda63356d7496ec85f5",
+    "url": "/static/media/remove-icon.020dfd90.svg"
+  },
+  {
+    "revision": "ddc2c0411fa638d08f170461c7c9ad90",
+    "url": "/static/media/rubbish.ddc2c041.svg"
+  },
+  {
+    "revision": "0c9f4dec3f32090efa0f8309680c7cba",
+    "url": "/static/media/search-icon.0c9f4dec.svg"
+  },
+  {
+    "revision": "f1261f5a06516f1a9957a2a1e1020e7d",
+    "url": "/static/media/travel.f1261f5a.svg"
+  },
+  {
+    "revision": "47be60baa8a7281a95b735338d2d4b18",
+    "url": "/static/media/user.47be60ba.svg"
+  }
+]);
